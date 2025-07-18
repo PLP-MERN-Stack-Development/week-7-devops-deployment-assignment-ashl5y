5613a4c11bb3d7aafb4a90574b90ddc61c2a9d0e
ashl5y <ashleypaula22@gmail.com>
2025-07-18 23:21:05 +0300

    User model line removal

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -48,7 +48,7 @@ UserSchema.pre('save', async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
-n
+
 UserSchema.methods.getSignedJwtToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRE,
